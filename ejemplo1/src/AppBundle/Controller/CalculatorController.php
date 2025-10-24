10cfac7c7bc2844bad3ae7961f67e85768807446
IsaacGonade <isaac.gonade@educa.jcyl.es>
2025-10-24 21:39:32 +0200

    imports

--- ejemplo1/src/AppBundle/Controller/CalculatorController.php
+++ ejemplo1/src/AppBundle/Controller/CalculatorController.php
@@ -3,7 +3,10 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class CalculatorController extends Controller
 {
